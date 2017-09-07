35887551d7827b4af771a42cc79e7265688579ad
Alexander Shniperson <alex.shniperson@gmail.com>
2017-09-07 19:59:05 +0400

    #4 fix

--- src/main/scala/ru/rknrl/rpc/WebSocketClientSession.scala
+++ src/main/scala/ru/rknrl/rpc/WebSocketClientSession.scala
@@ -10,6 +10,7 @@ package ru.rknrl.rpc
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
 import akka.http.scaladsl.model.ws.BinaryMessage
+import akka.stream.ActorMaterializer
 import akka.util.ByteString
 import com.trueaccord.scalapb.GeneratedMessage
 import ru.rknrl.rpc.WebSocketServer.{WebClientConnected, WebClientDisconnected}
@@ -22,21 +23,21 @@ object WebSocketClientSession {
 
 private class WebSocketClientSession(acceptWithActor: ActorRef ⇒ Props,
                                      serializer: Serializer) extends Actor with SocketMessage with ActorLogging {
-  var clientRef: Option[ActorRef] = None
+  var clientRef = Option.empty[ActorRef]
   val serverRef = context.actorOf(acceptWithActor(self))
   var receiveBuffer = ByteString.empty
+  implicit val materializer = ActorMaterializer()
 
   def receive = {
     case msg: GeneratedMessage ⇒ clientRef.foreach(_ ! BinaryMessage.Strict(buildMessage(msg, serializer)))
 
     case BinaryMessage.Strict(receivedData) ⇒
-      val data = receiveBuffer ++ receivedData
-      val (newBuffer, frames) = extractFrames(data, Nil)
-      for (frame ← frames) serverRef ! serializer.bytesToMessage(frame.msgId, frame.byteString)
-      receiveBuffer = newBuffer
+      parseReceivedData(receivedData)
 
-    case _: BinaryMessage.Streamed ⇒
-      log.warning("BinaryMessage.Streamed not supported")
+    case BinaryMessage.Streamed(receivedData) ⇒
+      receivedData.runForeach { data ⇒
+        parseReceivedData(data)
+      }
 
     case WebClientConnected(a: ActorRef) ⇒
       clientRef = Some(a)
@@ -55,4 +56,13 @@ private class WebSocketClientSession(acceptWithActor: ActorRef ⇒ Props,
 
   override def postStop(): Unit =
     clientRef.foreach(context.stop)
+
+  def parseReceivedData(receivedData: ByteString): Unit = {
+    val data = receiveBuffer ++ receivedData
+    val (newBuffer, frames) = extractFrames(data, Nil)
+    for (frame ← frames) {
+      serverRef ! serializer.bytesToMessage(frame.msgId, frame.byteString)
+    }
+    receiveBuffer = newBuffer
+  }
 }
